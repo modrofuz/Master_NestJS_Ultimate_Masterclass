69a3ca9e71eb8d9333afdb65ea7248d8c082ff80
dusanboocz <dusan.boocz@sk.ibm.com>
2025-10-31 13:33:54 +0100

    messageformatter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageFormatterService } from './services/message-formatter/message-formatter.service';
+import { LoggerService } from './services/logger/logger.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MessageFormatterService, LoggerService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './services/logger/logger.service';
+
 
 @Injectable()
 export class AppService {
+  constructor(private readonly loggerService: LoggerService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.loggerService.log('Hello World!');
   }
 }
